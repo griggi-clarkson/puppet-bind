460f764cc245d2b6d8a8ad15d9325d529ec334a0
Gavin Riggi <griggi@clarkson.edu>
2021-11-30 16:02:19 -0500

    adjust prefetch

--- lib/puppet/provider/resource_record/resource_record.rb
+++ lib/puppet/provider/resource_record/resource_record.rb
@@ -73,7 +73,7 @@ Puppet::Type.type(:resource_record).provide(:ruby) do
 
   def self.prefetch(resources)
     instances.each do |prov|
-      if (resource = resources[prov.name])
+      if (resource = resources[prov.title])
         resource.provider = prov
       end
     end
